235d75f6e0fe8074013aead256c624ba729fefd2
MikoStellio <kevinj.bernabe@gmail.com>
2025-08-22 12:56:21 +0800

    Updated test runner batch file to correct paths issue with environment hooks in before_all

--- behave_package/test_runner/run_behave_tests.bat
+++ behave_package/test_runner/run_behave_tests.bat
@@ -1,8 +1,8 @@
 cd ../../..
 call .venv\Scripts\activate.bat
 
-cd appium_demo/behave_package
+cd appium_demo
 
-behave behave/features -o test_reports/behave_report.txt
+behave behave_package/behave/features -o test_reports/behave_report.txt
 
 pause
